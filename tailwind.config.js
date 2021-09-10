21470393651d27db40dc4ea340af5b99ea6c3bf2
Wiscaksono <wwicaksono96@gmail.com>
2021-09-10 17:15:32 +0700

    fixing images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        darkgray: "#8C8C8C",
+      },
+
       backgroundImage: (theme) => ({
         background: "url('/images/background.svg')",
       }),
