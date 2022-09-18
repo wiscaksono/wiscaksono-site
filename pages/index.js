b2b190b885c429b9f9b88543dd3367f45dca6da7
Wisnu Wicaksono <63142229+wiscaksono@users.noreply.github.com>
2022-09-18 23:46:09 +0700

    optimized image

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,6 @@ import { Autoplay } from "swiper";
 import { useTheme } from "next-themes";
 import { useState, useEffect, Fragment } from "react";
 import "swiper/css";
-import Link from "next/link";
-import axios from "axios";
 import Head from "next/head";
 import { projects } from "/json/data.json";
 import { Dialog, Transition } from "@headlessui/react";
@@ -62,46 +60,44 @@ export default function Home() {
             <a href="#main" className="text-4xl font-extrabold">
               wsn.
             </a>
-            <div className="flex flex-col items-end">
-              <h6 className="font-semibold">self service</h6>
-              <p className="font-light text-xs">20% OFF</p>
-            </div>
+            <h6 className="font-semibold">portofolio</h6>
           </nav>
           <div className="w-[95%] relative flex items-center justify-center h-[calc(100vh-90px)] mx-auto">
-            <Typewriter
-              options={{
-                loop: true,
-                wrapperClassName: "text-5xl dot-gothic",
-                cursorClassName: "text-5xl dot-gothic",
-              }}
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString("Wisnu Wicaksono")
-                  .pauseFor(1000)
-                  .deleteAll()
-                  .typeString("front-end.web(developer)_")
-                  .pauseFor(500)
-                  .start();
-              }}
-            />
-            <ul className="flex flex-col gap-5 absolute left-0">
-              <li className="writing-mode-tb -rotate-180 text-sm">
+            <div>
+              <Typewriter
+                options={{
+                  loop: true,
+                  wrapperClassName: "lg:text-5xl text-xl dot-gothic",
+                  cursorClassName: "lg:text-5xl text-xl dot-gothic",
+                }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Wisnu Wicaksono")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("front-end.web(developer)_")
+                    .pauseFor(500)
+                    .start();
+                }}
+              />
+            </div>
+            <ul className="flex md:flex-col gap-5 absolute md:left-0 left-auto top-5 md:top-auto flex-wrap justify-center">
+              <li className="writing-mode-tb-nav text-sm">
                 <a href="#main">home</a>
               </li>
-              <li className="writing-mode-tb -rotate-180 text-sm">
+              <li className="writing-mode-tb-nav text-sm">
                 <a href="#cases">cases</a>
               </li>
-              <li className="writing-mode-tb -rotate-180 text-sm">
+              <li className="writing-mode-tb-nav text-sm">
                 <a href="#projects">projects</a>
               </li>
-              <li className="writing-mode-tb -rotate-180 text-sm">
+              <li className="writing-mode-tb-nav text-sm">
                 <a href="#about-me">about me</a>
               </li>
-              <li className="writing-mode-tb -rotate-180 text-sm">
+              <li className="writing-mode-tb-nav text-sm">
                 <a href="#contact">contact</a>
               </li>
             </ul>
-
             {renderThemeChanger()}
           </div>
         </div>
@@ -115,17 +111,28 @@ export default function Home() {
             </div>
 
             <Swiper
-              slidesPerView={4}
+              slidesPerView={1}
               spaceBetween={20}
               modules={[Autoplay]}
               autoplay={{
                 delay: 2000,
               }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 50,
+                },
+              }}
+              grabCursor={true}
             >
-              {projects.map((project, idx) => {
+              {projects.map((data, idx) => {
                 return (
                   <SwiperSlide key={idx}>
-                    <ModalImage image={project.image} alt="image" />
+                    <ModalImage image={data.image} alt="image" />
                   </SwiperSlide>
                 );
               })}
@@ -357,150 +364,16 @@ export default function Home() {
 }
 
 function ModalImage({ image }) {
-  let [isOpen, setIsOpen] = useState(false);
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
   return (
     <>
-      <button onClick={openModal}>
-        <Image
-          src={`/projects/${image}`}
-          width={391}
-          height={356}
-          className="object-cover"
-          alt="aoskdasd"
-        />
-      </button>
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 max-w-2xl flex items-center justify-center">
-                  <img
-                    src={`/projects/${image}`}
-                    className="w-max h-max"
-                    alt={`oaskdas`}
-                  />
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
+      <Image
+        src={`/projects/${image}`}
+        width={391}
+        height={356}
+        className="object-cover"
+        quality={80}
+        alt="Image"
+      />
     </>
   );
 }
-
-const Project = ({ judul, desc, url, image }) => {
-  let [isOpen, setIsOpen] = useState(false);
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function imageCheck(value) {
-    if (value === "null") {
-      return "/placeholder-image.webp";
-    } else {
-      return value;
-    }
-  }
-
-  return (
-    <div className="flex gap-9 flex-col lg:flex-row">
-      <button
-        className="lg:w-[233px] lg:h-[233px] h-[300px] bg-[#F9F9F9] flex-shrink-0 flex items-center justify-center relative"
-        onClick={openModal}
-      >
-        <img
-          src={imageCheck(image)}
-          alt=""
-          className="object-cover w-full h-full"
-        />
-        <div className="absolute bottom-3 right-3">
-          <p className="bg-white rounded-full text-[10px] py-[1px] px-2 font-extrabold">
-            HTML
-          </p>
-        </div>
-      </button>
-      <article className="flex flex-col justify-between">
-        <div>
-          <h4 className="font-extrabold text-xl mb-1">{judul}</h4>
-          <desc className="text-[#808080] font-light lg:line-clamp-5 line-clamp-3">
-            {desc}
-          </desc>
-        </div>
-        <a href={url} target={"_blank"} rel="noreferrer" className="w-max">
-          <span>see project</span>
-          <div className="w-6 h-[2px] bg-black dark:bg-white " />
-        </a>
-      </article>
-
-      {/* Open Modal */}
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 max-w-2xl flex items-center justify-center">
-                  <img src={image} className="w-max h-max" alt={judul} />
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </div>
-  );
-};
