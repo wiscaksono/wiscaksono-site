66434004f20cfabf6b8461b84bd550ed1831f083
Wisnu Wicaksono <wwicaksono96@gmail.com>
2023-11-08 23:41:28 +0700

    enhance: change gif to webp

--- src/components/PageLoading.tsx
+++ src/components/PageLoading.tsx
@@ -2,8 +2,8 @@ import { Loader2 } from "lucide-react";
 
 export default function PageLoading() {
   return (
-    <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+    <div className="flex-1 flex items-center justify-center">
       <Loader2 className="w-10 h-10 animate-spin text-orange" />
     </div>
-  )
+  );
 }
