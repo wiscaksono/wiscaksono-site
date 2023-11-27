311a8ef447ef5f9c996ffd7620ffd25e949d2328
Wisnu Wicaksono <wisnu.wicaksono@msiglife.com>
2023-11-28 06:48:22 +0700

    chore: cleanup unused codes

--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -1,12 +1,4 @@
 import Link from 'next/link'
-import { SiTypescript } from 'react-icons/si'
-import { FaMarkdown } from 'react-icons/fa'
-import { RiReactjsLine, RiHtml5Fill } from 'react-icons/ri'
-import { TbBrandNextjs } from 'react-icons/tb'
-import { HiTerminal } from 'react-icons/hi'
-import { FaRegEnvelope } from 'react-icons/fa'
-import { BsWhatsapp, BsInstagram, BsLinkedin } from 'react-icons/bs'
-import { TbBrandUpwork } from 'react-icons/tb'
 
 import { NavLink } from '@/components/atoms/nav-link'
 import { ThemeToggle } from '@/components/molecules/theme-toggler'
@@ -41,80 +33,18 @@ export const navMenu = [
   },
   {
     name: '_about-me',
-    path: '/about',
-    child: [
-      {
-        name: 'personal.ts',
-        path: '/about/personal.ts',
-        icon: <SiTypescript className='w-4 h-4 shrink-0' />
-      },
-      {
-        name: 'work.ts',
-        path: '/about/work',
-        icon: <SiTypescript className='w-4 h-4 shrink-0' />
-      },
-      {
-        name: 'gear.md',
-        path: '/about/gear',
-        icon: <FaMarkdown className='w-4 h-4 shrink-0' />
-      }
-    ]
+    path: '/about'
   },
   {
     name: '_projects',
-    path: '/projects',
-    child: [
-      {
-        name: 'All Projects',
-        path: '/projects',
-        icon: <HiTerminal className='w-4 h-4' />
-      },
-      {
-        name: 'React',
-        path: '/projects?tag=react',
-        icon: <RiReactjsLine className='w-4 h-4' />
-      },
-      {
-        name: 'Next',
-        path: '/projects?tag=next',
-        icon: <TbBrandNextjs className='w-4 h-4' />
-      },
-      {
-        name: 'HTML',
-        path: '/projects?tag=html',
-        icon: <RiHtml5Fill className='w-4 h-4' />
-      }
-    ]
+    path: '/projects'
   },
   {
     name: '_guest-book',
-    path: '/guest-book',
-    child: [
-      {
-        name: 'Email',
-        path: 'mailto:wwicaksono96@gmail.com',
-        icon: <FaRegEnvelope className='w-4 h-4' />
-      },
-      {
-        name: 'Upwork',
-        path: 'https://www.upwork.com/freelancers/~01df34d78e05fa69bf',
-        icon: <TbBrandUpwork className='w-4 h-4' />
-      },
-      {
-        name: 'WhatsApp',
-        path: 'https://wa.me/+6287885002327',
-        icon: <BsWhatsapp className='w-4 h-4' />
-      },
-      {
-        name: 'LinkedIn',
-        path: 'https://www.linkedin.com/in/wiscaksono/',
-        icon: <BsLinkedin className='w-4 h-4' />
-      },
-      {
-        name: 'Instagram',
-        path: 'https://www.instagram.com/amachoker/',
-        icon: <BsInstagram className='w-4 h-4' />
-      }
-    ]
+    path: '/guest-book'
+  },
+  {
+    name: '_articles',
+    path: '/articles'
   }
 ]
