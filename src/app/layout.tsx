c16305bdd11601fb82ef51190842b3537451b36a
Wisnu Wicaksono <wisnu.wicaksono@msiglife.com>
2023-12-08 05:10:08 +0700

    feat: add vercel speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { ThemeWrapper } from '@/components/atoms/theme-wrapper'
 import { Navbar } from '@/components/organisms/navbar'
@@ -61,7 +62,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Footer />
           </main>
         </ThemeWrapper>
-        {process.env.NODE_ENV === 'production' && <Analytics />}
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <Analytics />
+            <SpeedInsights />
+          </>
+        )}
       </body>
       <ResponsiveIndicator />
     </html>
