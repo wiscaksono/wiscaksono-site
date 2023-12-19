b335e2e4c61ecdb6e8dbe6c1bd942fd9b43010e4
Wisnu Wicaksono <wisnu.wicaksono@msiglife.co.id>
2023-12-20 01:45:19 +0700

    feat: update tailwind 3.4, h-screen -> h-dvh

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <section className='h-screen flex items-center justify-center flex-col'>
+    <section className='h-dvh flex items-center justify-center flex-col'>
       <div className='relative text-center'>
         <h1 className='mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-5xl'>Page not found :(</h1>
         <p className='mt-6 text-base leading-7 text-off-white'>Sorry, i couldn’t find the page you’re looking for.</p>
