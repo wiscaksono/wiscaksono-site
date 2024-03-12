648b63f476b81e19fafb249bf000e8910d674c33
Wisnu Wicaksono <wisnu.wicaksono@msiglife.co.id>
2024-03-12 19:30:14 +0700

    fix: change fonts to sans in project detail

--- src/app/projects/[title]/page.tsx
+++ src/app/projects/[title]/page.tsx
@@ -43,7 +43,7 @@ export default async function ProjectDetail({ params }: { params: ParamsProps })
     <MDXComponent
       code={about.body.code}
       className={
-        'prose-h2:text-foreground prose-h3:text-foreground prose-p:text-muted-foreground prose-a:text-foreground prose-li:text-muted-foreground prose-img:w-2/3 prose-img:mx-auto prose-strong:text-foreground prose-h4:text-foreground prose-a:no-underline'
+        'prose-h2:text-foreground font-sans prose-h3:text-foreground prose-p:text-muted-foreground prose-a:text-foreground prose-li:text-muted-foreground prose-img:w-2/3 prose-img:mx-auto prose-strong:text-foreground prose-h4:text-foreground prose-a:no-underline'
       }
     />
   )
