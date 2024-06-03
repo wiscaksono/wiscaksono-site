9faba8a0f90f1f47685f0b8bb3169e2c09cfb046
Wisnu Wicaksono <wisnu.wicaksono@msiglife.co.id>
2024-06-03 19:08:04 +0700

    feat: re-adding umami tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Metadata } from 'next'
+import Script from 'next/script'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
 
@@ -81,6 +82,7 @@ export default async function RootLayout({ children }: Readonly<Props>) {
           }}
         />
         <div className='grid-pattern absolute left-0 top-0 h-full w-full' />
+        <Script defer src='https://umami.wiscaksono.com/script.js' data-website-id='1f3b0505-7366-47bd-8757-95ad25395088' />
       </body>
     </html>
   )
