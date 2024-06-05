1782b0e5ae156d039d9761e2fe31775d04fc51e6
Wisnu Wicaksono <wisnu.wicaksono@msiglife.co.id>
2024-06-06 03:20:09 +0700

    fix: wordings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <pre className='flex h-full flex-col items-center justify-center space-y-2.5 whitespace-pre-wrap md:space-y-5'>
       <code className='text-[2dvw] leading-[0.9] tracking-[-0.1em] lg:text-[1.4dvh]'>{ascii2}</code>
       <code className='text-center text-sm md:text-base'>
-        <p>Self tought web developer</p>
+        <p>Self taught web developer</p>
         <p>love to learn new things and I&apos;m always looking for new challenges to solve :)</p>
       </code>
     </pre>
