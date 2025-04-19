8d5d7e01ad14dae5a4a053036ba6a5881c950753
Wiscaksono <wwicaksono96@gmail.com>
2025-04-19 18:08:14 +0700

    fix(about): change redirect status from 307 to 308

--- src/routes/abouts/+page.server.ts
+++ src/routes/abouts/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	return redirect(307, '/abouts/personal');
+	return redirect(308, '/abouts/personal');
 };
